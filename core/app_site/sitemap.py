8de588fdacf21c990dc1b75171450f520908992b
Kucheriavenko Dmitrii <Kucherdimdimych@mail.ru>
2024-09-12 01:10:24 +0500

    add infinite scroll and partly dark mode

--- core/app_site/sitemap.py
+++ core/app_site/sitemap.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import sitemaps
 from django.urls import reverse
 
@@ -12,20 +11,3 @@ class StaticViewSitemap(sitemaps.Sitemap):
 
     def location(self, item):
         return reverse(item)
-
-=======
-from django.contrib import sitemaps
-from django.urls import reverse
-
-
-class StaticViewSitemap(sitemaps.Sitemap):
-    changefreq = 'monthly'
-    priority = 0.9
-
-    def items(self):
-        return ['app_site:index']
-
-    def location(self, item):
-        return reverse(item)
-
->>>>>>> 6110949cb381fe83926376cf17ed9f97b0b73a67
--- core/core/settings.py
+++ core/core/settings.py
@@ -1,109 +1,3 @@
-<<<<<<< HEAD
-import os
-from pathlib import Path
-from os import environ
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sitemaps',
-    'django.contrib.sites',
-    'app_site',
-]
-SITE_ID = 1
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'core.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'core.wsgi.application'
-
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-LANGUAGE_CODE = 'ru-ru'
-
-TIME_ZONE = 'Asia/Omsk'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_URL = '/assets/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# DEFAULT_FROM_EMAIL = 'user@domain.com'
-
-try:
-    from .local_settings import *
-except ImportError:
-    from .prod_settings import *
-
-#  DebugToolbar
-if DEBUG:
-    MIDDLEWARE += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    )
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
-=======
 import os
 from pathlib import Path
 from os import environ
@@ -186,12 +80,12 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 if DEBUG:
-    STATIC_DIR = os.path.join(BASE_DIR, 'static')
+    STATIC_DIR = os.path.join(BASE_DIR, 'assets')
     STATICFILES_DIRS = [STATIC_DIR]
 else:
-    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_ROOT = BASE_DIR / 'assets'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -231,4 +125,3 @@ CELERY_TIMEZONE = os.getenv("CELERY_TIMEZONE")
 
 TOKEN_TG = os.getenv("TG_TOKEN")
 CHAT_ID =  os.getenv("TG_CHAT_ID")
->>>>>>> 6110949cb381fe83926376cf17ed9f97b0b73a67
